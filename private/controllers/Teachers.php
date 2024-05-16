cf297d1cb160e07488a880ff06e467aa5968ed65
youssef-elkahlaoui <youssefelkahlaoui18@gmail.com>
2024-05-16 19:08:46 +0100

    prof files

---
+++ private/controllers/Teachers.php
@@ -0,0 +1,59 @@
+<?php 
+
+class Students extends Controller
+{
+    function index()
+	{
+        		// code...
+		if(!Auth::teacherLoggedIn())
+		{
+			$this->redirect('login');
+		}
+		$user = new User();
+
+		$data = $user->findAll();
+
+		$this->view('home.prof',['rows'=>$data]);
+    }
+	
+    function absence(){
+        $this->view('absence.prof');
+    }
+
+	function classe(){
+        $this->view('classe.etu');
+    }
+
+	function demande(){
+        $this->view('demande.prof');
+    }
+
+	function note(){
+        $this->view('note.prof');
+    }
+
+	function listeProf(){
+        $this->view('listeProf');
+    }
+
+	function etatDemande(){
+        $this->view('demande.etat.prof');
+    }
+
+	
+    function documentation(){
+        $this->view('documentation.prof');
+    }
+
+	function devoir(){
+        $this->view('devoir.prof');
+    }
+    
+    function modules(){
+        $this->view("modules");
+    }
+
+    function profile($id = null){
+        $this->view("profile");
+    }
+}
