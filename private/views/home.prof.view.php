a60d09a148a751dffbf062b7c815d0153529dcf8
youssef-elkahlaoui <youssefelkahlaoui18@gmail.com>
2024-05-21 16:05:19 +0100

    kjnk

--- private/views/home.prof.view.php
+++ private/views/home.prof.view.php
@@ -1,8 +1,8 @@
 <?php 
     include "includes/nav.prof.view.php";
     include "includes/header.view.php";
-class Login extends Controller
-{
+    class Login extends Controller
+    {
   function index()
   {
       $errors = array();
@@ -137,11 +137,9 @@ class Login extends Controller
 
       return json_encode($last4WeeklyCounts);
   }
-}
-
-// In your HTML view:
-$loginController = new Login();
-$last4WeeklyCounts = $loginController->getChartData();
+    }
+    $loginController = new Login();
+    $last4WeeklyCounts = $loginController->getChartData();
 ?>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -167,7 +165,7 @@ $last4WeeklyCounts = $loginController->getChartData();
                   <div class="row">
                     <div class="col">
                       <h5 class="card-title text-uppercase text-muted mb-0">Traffic</h5>
-                      <a href="#"><span class="h2 font-weight-bold mb-0">897</span></a>
+                      <a href="#"><span class="h2 font-weight-bold mb-0 text-danger">897</span></a>
                     </div>
                     <div class="col-auto">
                       <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
@@ -175,8 +173,6 @@ $last4WeeklyCounts = $loginController->getChartData();
                     </div>
                   </div>
                   <p class="mt-3 mb-0 text-muted text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
                   </p>
                 </div>
               </div>
@@ -187,7 +183,7 @@ $last4WeeklyCounts = $loginController->getChartData();
                   <div class="row">
                     <div class="col">
                       <h5 class="card-title text-uppercase text-muted mb-0">New notifications</h5>
-                      <a href="#"><span class="h2 font-weight-bold mb-0">356</span></a>
+                      <a href="#"><span class="h2 font-weight-bold mb-0 text-success">356</span></a>
                     </div>
                     <div class="col-auto">
                       <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
@@ -195,8 +191,6 @@ $last4WeeklyCounts = $loginController->getChartData();
                     </div>
                   </div>
                   <p class="mt-3 mb-0 text-muted text-sm">
-                    <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
                   </p>
                 </div>
               </div>
@@ -206,8 +200,8 @@ $last4WeeklyCounts = $loginController->getChartData();
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">Documents</h5>
-                      <a href="#"><span class="h2 font-weight-bold mb-0">924</span></a>
+                      <h5 class="card-title text-uppercase text-muted mb-0 ">Documents</h5>
+                      <a href="#"><span class="h2 font-weight-bold mb-0 text-warning ">924</span></a>
                     </div>
                     <div class="col-auto">
                       <div class="icon icon-shape bg-yellow text-white rounded-circle shadow">
@@ -215,8 +209,6 @@ $last4WeeklyCounts = $loginController->getChartData();
                     </div>
                   </div>
                   <p class="mt-3 mb-0 text-muted text-sm">
-                    <span class="text-warning mr-2"><i class="fas fa-arrow-down"></i> 1.10%</span>
-                    <span class="text-nowrap">Since last year</span>
                   </p>
                 </div>
               </div>
@@ -226,7 +218,7 @@ $last4WeeklyCounts = $loginController->getChartData();
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">Logins</h5>
+                      <h5 class="card-title text-uppercase text-muted mb-0 text-info">Logins </h5>
                       <a href="#"><span class="h2 font-weight-bold mb-0">49,65%</span></a>
                     </div>
                     <div class="col-auto">
@@ -235,8 +227,6 @@ $last4WeeklyCounts = $loginController->getChartData();
                     </div>
                   </div>
                   <p class="mt-3 mb-0 text-muted text-sm">
-                    <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
-                    <span class="text-nowrap">Since last month</span>
                   </p>
                 </div>
               </div>
