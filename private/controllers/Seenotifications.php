89911fb6cc376cf2353b29e94271ca5bcb83319b
youssef-elkahlaoui <youssefelkahlaoui18@gmail.com>
2024-05-25 10:51:12 +0100

    lljsp

---
+++ private/controllers/Seenotifications.php
@@ -0,0 +1,20 @@
+<?php
+class Seenotifications extends Controller {
+    public function scroll() {
+        try {
+            $db = new Database(); 
+            $query = "SELECT message FROM notifications ORDER BY DateNotification DESC LIMIT 4"; 
+            $notifications = $db->query($query, [], "assoc");
+        } catch (PDOException $e) {
+            error_log("Error: " . $e->getMessage());
+            $notifications = [];
+        }
+        return $notifications;
+    }
+
+    public function index() {
+        $notifications = $this->scroll(); 
+        $this->view('home.prof', ['notifications' => $notifications]); 
+    }
+}
+?>
