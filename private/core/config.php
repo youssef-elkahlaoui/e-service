e38deadc61c697ca13301ebfdcfe0881a1927a2b
youssef-elkahlaoui <146426121+youssef-elkahlaoui@users.noreply.github.com>
2024-06-03 16:22:07 +0100

    Update config.php

--- private/core/config.php
+++ private/core/config.php
@@ -3,7 +3,7 @@
 define('ROOT', 'http://localhost/e-service/public');
 define('ASSETS', 'http://localhost/e-service/public/assets');
 
-define('DBNAME', 'backup');
+define('DBNAME', 'id22197871_school_db2');
 define('DBHOST', 'localhost');
 define('DBUSER', 'root');
 define('DBPASS', '');
